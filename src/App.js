b2e2a9e396ddaf20580f19608da9e21f83e99afe
Shrey <shreyshukla1010@gmail.com>
2024-09-24 17:35:59 +0530

    fixing prop drilling

--- src/App.js
+++ src/App.js
@@ -50,18 +50,35 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-function NavBar({movies}){
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
+    </>
+  );
+}
+
+function NavBar({children}){
 
   return (
     <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults/>
+      {children}
     </nav>
   );
 }
 
-function NumResults(){
+function NumResults({movies}){
   return (
     <p className="num-results">
       Found <strong>{movies.length}</strong> results
@@ -91,21 +108,10 @@ function Search(){
   );
 }
 
-export default function App() {
-
-  return (
-    <>
-    <NavBar movies={movies} />
-      <Main />
-    </>
-  );
-}
-
-function Main(){
+function Main({children}){
 
   return (<main className="main">
-    <ListBox />
-    <WatchedBox/>
+    {children}
   </main>)
 }
 
@@ -195,7 +201,7 @@ function WatchedSummary({watched}){
 )
 }
 
-function ListBox(){
+function ListBox({children}){
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -206,14 +212,13 @@ function ListBox(){
         {isOpen1 ? "–" : "+"}
       </button>
       {isOpen1 && (
-        <MovieList/>
+        children
       )}
     </div>
   );
 }
 
-function MovieList(){
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({movies}){
   return (
     <ul className="list">
       {movies?.map((movie) => (
