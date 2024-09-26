b7acc21601c081dd7363165aaa67c0dc27dd9e73
Shrey <shreyshukla1010@gmail.com>
2024-09-26 17:42:45 +0530

    taking user rating via stars

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-      <App />
+      {/* <App /> */}
       <StarRating maxRating={5} />
     </>
   </React.StrictMode>
