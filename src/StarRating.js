86df6495ba49bc90fc5d00de1b340cfe27a25637
Shrey <shreyshukla1010@gmail.com>
2024-09-27 19:53:35 +0530

    PropTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,7 +1,17 @@
 import { useState } from "react"
+import PropTypes from "prop-types"
 
 
-export default function StarRating({maxRating = 10,color = '#fcc419' , size=48, className="" , messages=[] , onSetRating,  }){
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  onSetRating : PropTypes.func
+}
+
+export default function StarRating({maxRating = 10,color = '#fcc419' , size=48, className="" , messages=[] , onSetRating }){
 
     const containerStyle ={display:"flex",alignItems:"center",gap:"16px"}
     const starContainerStyle ={display:"flex",gap:"4px"}
@@ -10,7 +20,7 @@ export default function StarRating({maxRating = 10,color = '#fcc419' , size=48, 
     const [rating,setRating] = useState(0)
     const [tempRating , setTempRating] = useState(0)
 
-    function handleRating(){
+    function handleRating(i){
       setRating(i + 1);
       onSetRating(i+1);
     }
@@ -19,7 +29,7 @@ export default function StarRating({maxRating = 10,color = '#fcc419' , size=48, 
       <div style={containerStyle} className={className}>
         <div style={starContainerStyle}>
           {Array.from({ length: maxRating }, (_, i) => (
-            <Star key = {i} onRate={()=>handleRating} full ={tempRating ? tempRating >=i+1 :rating>=i+1} onHoverIn={()=>setTempRating(i+1)} onHoverOut={()=>setTempRating(0)} color={color} size={size} />
+            <Star key = {i} onRate={()=>handleRating(i)} full ={tempRating ? tempRating >=i+1 :rating>=i+1} onHoverIn={()=>setTempRating(i+1)} onHoverOut={()=>setTempRating(0)} color={color} size={size} />
           ))}
         </div>
         <p style={textStyle}>{ messages.length === maxRating ? messages[ tempRating ? tempRating -1 :rating-1] : tempRating || rating || ""}</p>
